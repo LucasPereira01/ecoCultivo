f674f71f8efb72458e09e86bf1601319357b921c
LucasPereira01 <lucaspdocarmo@gmail.com>
2024-05-08 14:07:57 -0300

    Projeto integrador

---
+++ apiClimatempo.js
@@ -0,0 +1,12 @@
+// "http://apiadvisor.climatempo.com.br/api/v1/locale/city?name=São Paulo&state=SP&token=your-app-token"
+
+
+/*curl -X PUT \
+        http://apiadvisor.climatempo.com.br/api-manager/user-token/6fb01449a637457ea38b3f0d9aa9879f/locales
+     'http://apiadvisor.climatempo.com.br/api-manager/user-token/:your-app-token/locales' \
+         -H 'Content-Type: application/x-www-form-urlencoded' \
+         -d 'localeId[]=3477'*/
+
+
+
+//         http://apiadvisor.climatempo.com.br/api/v1/weather/locale/4371/current?token=6fb01449a637457ea38b3f0d9aa9879f
